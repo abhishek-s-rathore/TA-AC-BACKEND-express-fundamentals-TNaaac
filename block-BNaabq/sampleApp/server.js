3c11a0cebc1738cadda88e95958245719200eafe
Abhishek <abhishek.990914@gmail.com>
2021-08-14 10:55:15 +0530

    block refectored

--- block-BNaabq/sampleApp/server.js
+++ block-BNaabq/sampleApp/server.js
@@ -2,21 +2,37 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+// Request and response generation
 let app = express();
 
+// middlewares
+//In-built
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + `/assets`));
+
+//Third-party
 app.use(logger('dev'));
+
+// Custom made
+
 app.use(cookieParser());
 
-app.use((req, res, next) => {
-  console.log(req.cookie);
-  var username = 'abhi_1234';
-  res.cookie('username', username);
+app.use('/', (req, res, next) => {
+  console.log(req.cookies);
   next();
 });
 
-app.get('/', (req, res) => {
-  res.send('Welcome to home page!');
+app.use('/about', (req, res, next) => {
+  res.cookie('username', 'abhi_1234');
+  res.send('About Page');
+  next();
 });
-app.listen(6666, 'localhost', () => {
-  console.log('Server listning to port 6666!');
+
+// Routing Middlewares
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+app.listen(6000, 'localhost', () => {
+  console.log('Server listning to port 6000!');
 });
