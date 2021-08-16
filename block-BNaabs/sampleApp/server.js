70a3a293b99a1a463d8d2a9f6de9d9db568075e6
Abhishek <abhishek.990914@gmail.com>
2021-08-16 11:45:46 +0530

    block refectored

--- block-BNaabs/sampleApp/server.js
+++ block-BNaabs/sampleApp/server.js
@@ -4,10 +4,10 @@ const cookieParser = require('cookie-parser');
 
 let app = express();
 
-
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: false }));
 
+app.use(logger('dev'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -18,9 +18,14 @@ app.get('/new', (req, res) => {
 });
 
 app.post('/new', (req, res) => {
-    
-    
-  });
+  console.log(req.body);
+  res.json(req.body);
+});
+
+app.get('/users/:username', (req, res) => {
+  var username = req.params.username;
+  res.send(username);
+});
 
 app.listen(1234, 'localhost', () => {
   console.log('Server listning to port 1234!');
